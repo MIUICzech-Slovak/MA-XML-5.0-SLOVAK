7198f93d9ff1bcdc23f9d203708f93ddfb56cb9c
ingbrzy <ingbrzy@ingbrzy.eu>
2014-05-14 20:24:25 +0200

    XML 4.5.14

--- add_lines.h
+++ add_lines.h
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -name "*.xml" -exec sed -i -e '2i\<!--This Slovak translation is licenced under www.creativecommons.org\/licenses\/by-nc-nd\/3.0\/deed.en by MIUI CZ&SK www.miui.cz-->' {} \; > /dev/null 2>&1
+find . -name "strings.xml" -exec sed -i -e '2i\<!--This Slovak translation is licenced under www.creativecommons.org\/licenses\/by-nc-nd\/3.0\/deed.en by MIUI CZ&SK www.miui.cz-->' {} \; > /dev/null 2>&1
 
